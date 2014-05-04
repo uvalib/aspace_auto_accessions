d8ab161d2dbcef47ab23d1ba0480f0b972fb1433
Brian Hoffman <brianjhoffman@gmail.com>
2014-05-04 11:54:03 -0400

    create a department list for each existing repository when migrating

--- migrations/001_add_department_codes.rb
+++ migrations/001_add_department_codes.rb
@@ -2,17 +2,39 @@
 Sequel.migration do
 
   up do
-    create_table(:department) do
+    create_table(:department_list) do
       primary_key :id
 
-      String :code, :unique => true, :null => false
-      Integer :repo_id, :null => false
+      Integer :lock_version, :default => 0, :null => false
+      Integer :repo_id, :null => false, :unique => true
+      MediumBlobField :codes, :null => false
+      String :default_value, :null => true
 
       apply_mtime_columns
     end
 
-    alter_table(:department) do
+    alter_table(:department_list) do
       add_foreign_key([:repo_id], :repository, :key => :id)
+    end
+
+    # Create a blank list for each extant repository
+    self[:repository].all.each do |row|
+      id = row[:id]
+
+      values = {
+        :repo_id => id,
+        :codes => JSON([]),
+      }
+
+      [:create_time, :system_mtime, :user_mtime].each do |time|
+        values[time] = Time.now
+      end
+
+      [:created_by, :last_modified_by].each do |mod|
+        values[mod] = 'admin'
+      end
+
+      self[:department_list].insert(values)
     end
   end
 
