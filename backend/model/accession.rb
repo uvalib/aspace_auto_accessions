b7a3f729eebe7aa71aed9a8239757de8a9282217
Brian Hoffman <brianjhoffman@gmail.com>
2014-05-04 11:46:35 -0400

    finish id_0 and id_1 auto generators

--- backend/model/accession.rb
+++ backend/model/accession.rb
@@ -1,12 +1,52 @@
 Accession.class_eval do
 
+  alias_method :core_update_from_json, :update_from_json
+
+  def update_from_json(json, *other_args)
+    json[:id_2] = nil
+
+    # don't get a new sequence unless id_0 or id_1 has changed
+    if self[:identifier]
+      identifier = Identifiers.parse(self[:identifier])
+      if [identifier[0], identifier[1]] == [json[:id_0], json[:id_1]]
+        json[:id_2] = identifier[2]
+      end
+    end
+
+    core_update_from_json(json, *other_args)
+  end
+
+
   id_2_generator = lambda {|json|
-    sequence = "yale_accession_#{json['id_0']}_#{json['id_1']}"
-    Sequence.get(sequence).to_s
+    sequence_name = "yale_accession_#{json['id_0']}_#{json['id_1']}"
+
+    seq = Sequence.get(sequence_name)
+    seq = Sequence.get(sequence_name) if seq < 1
+
+    seq.to_s.rjust(4, '0')
   }
 
-  auto_generate :property => :id_2,
-							  :generator => id_2_generator,
-  						  :only_on_create => true
+  id_0_generator = lambda {|json|
+    date = Date.parse(json['accession_date'])
+    "#{date.month > 5 ? date.year : date.year - 1}"
+  }
+
+  # We can't use the built-in auto-generate helper because
+  # these generators needs to be first in line
+  properties_to_auto_generate.unshift ({
+    :property => :id_0,
+    :generator => id_0_generator,
+    :only_if => proc { true }
+  })
+
+  # override of update_from_json will set id_2
+  # from the db if nothing else has changed
+  properties_to_auto_generate.unshift ({
+    :property => :id_2,
+    :generator => id_2_generator,
+    :only_if => proc {|json| json[:id_2].nil? }
+  })
+
+
 
 end
--- frontend/assets/yale_accession_identifiers.js
+++ frontend/assets/yale_accession_identifiers.js
@@ -1,15 +1,19 @@
 $(function () {
 
   var $ids = $('div.identifier-fields');
-  var $id_0 = $('div.identifier-fields input#accession_id_0_');
+  var $id_0 = $('#accession_id_0_');
+  var $id_2 = $('#accession_id_2_');
 
-  $('#accession_id_2_').val('XXXX');
   $('#accession_id_3_').hide();
-  
 
   var init = function () {
     $ids.removeClass('required');
     $id_0.attr('readonly', 'readonly');
+    $id_2.attr('disabled', 'disabled');
+
+    if (! $id_2.val().length) {
+      $id_2.val('XXXX');
+    }
 
     var date = $('#accession_accession_date_').val();
     if (date.length) update_fiscal_year(date);
@@ -17,12 +21,30 @@ $(function () {
     $.ajax({
       url: APP_PATH + "plugins/yale_accessions/department_codes",
       data: {},
+      dataType: 'json',
       type: "GET",
-      success: function(codes) {
+      success: function(department_list) {
+        var codes = department_list.codes;
+
+        var current_code = $('#accession_id_1_').val();
+
+        // Deprecated department codes
+        if (current_code.length && $.inArray(current_code, codes) < 0) {
+          codes.push(false);
+          codes.push(current_code);
+        }
+
         if (codes.length > 1) {
-          var html = "<select id=\"accession_id_1_\" name=\"accession[id_1]\" style=\"width: 100px\">";
+          var html = "<select id=\"accession_id_1_\" name=\"accession[id_1]\"";
           $.each(codes, function(i, code) {
-            html += "<option value=\"" + code + "\">" + code + "</option>";
+            if (code == current_code) {
+              html += "<option value=\"" + code + "\" selected=\"selected\">" + code + "</option>";
+            } else if (code == false) {
+              html += "<option disabled>&#9472;</option>";
+            } else {
+              html += "<option value=\"" + code + "\">" + code + "</option>";
+            }
+
           });
 
           html += "</select>"
@@ -60,6 +82,7 @@ $(function () {
   $btn.datepicker().on("changeDate", function() {
     update_fiscal_year($fld.val());
   });
+
 
 })
 
